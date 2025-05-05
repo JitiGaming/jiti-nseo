9a2ec589be4da65e149aba8b178f941c9c8b8a0f
Jiti <7537443+JitiGaming@users.noreply.github.com>
2025-05-05 09:17:04 +0200

    Add files via upload

---
+++ jiti-nseo.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: Jiti - NSEO
+ * Description: Supprime les paramètres non autorisés sur toutes les pages du site.
+ * Version: 3.0
+ * Author: Jiti
+ * Author URI: https://jiti.me
+ * License: Copyleft
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+add_action( 'template_redirect', function() {
+    if ( is_admin() ) {
+        return;
+    }
+
+    $allowed_params = [ 'utm_source', 'utm_medium', 'utm_campaign', 's' ];
+
+    $current_query = $_GET;
+    $clean_query = array_intersect_key( $current_query, array_flip( $allowed_params ) );
+
+    $request_uri = isset($_SERVER['REQUEST_URI']) ? strtok($_SERVER['REQUEST_URI'], '?') : '/';
+    $new_url = home_url( $request_uri );
+
+    if ( !empty( $clean_query ) ) {
+        $new_url .= '?' . http_build_query( $clean_query );
+    }
+
+    $current_url = home_url( $_SERVER['REQUEST_URI'] );
+
+    if ( urldecode( $current_url ) !== urldecode( $new_url ) ) {
+        wp_redirect( $new_url, 301 );
+        exit;
+    }
+});
